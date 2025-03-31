62df180633bf165d726197c3d96a45f1287b5279
Mahadevan Arul <125157037@sastra.ac.in>
2025-03-31 15:51:23 +0530

    updated to use api keys securely

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // Import the necessary modules
 const express = require('express');
 const path = require('path');
-
+require("dotenv").config()
 // Create an instance of Express
 const app = express();
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname,'public')));
+
+//for env file 
+app.get("/config", (req, res) => {
+    res.json({ API_KEY: process.env.PINATA_API_KEY ,
+        API_SECRET: process.env.PINATA_API_SECRET ,
+    });
+  });
 
 // Route for the main page
 app.get('/', (req, res) => {
